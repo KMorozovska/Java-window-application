a7772bbba810c27b609d4eb6d0776b804a36dd72
KasiaSpa <kasia.spalinska@gmail.com>
2017-03-27 15:34:12 +0200

    Add files via upload
    sam początek, jeszcze bez kontenera na dane

---
+++ Date.java
@@ -0,0 +1,14 @@
+
+public class Date {
+	
+	int dzien;
+	int miesiac;
+	int rok;
+	
+	public Date(int d, int m, int r) {
+		this.dzien = d;
+		this.miesiac = m;
+		this.rok = r;
+	}
+
+}
---
+++ Pacjent.java
@@ -0,0 +1,26 @@
+
+public class Pacjent {
+	
+	String imie;
+	String nazwisko;
+	int pesel;
+	boolean plec;
+	int rodzaj_ubezpieczenia;
+	
+	public Pacjent(String i, String n, int p, boolean pl, int r) {
+		this.imie = i;
+		this.nazwisko = n;
+		this.pesel = p;
+		this.plec = pl;
+		this.rodzaj_ubezpieczenia = r;
+	}
+	
+	void getName() {
+		
+	}
+	
+	void setName() {
+		
+	}
+
+}
